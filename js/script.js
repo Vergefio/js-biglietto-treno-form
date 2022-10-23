584871a2b1763d36e33ced47b6a3c2afb9c45932
Vergefio <riccardods@live.it>
2022-10-23 13:15:31 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,14 +1,48 @@
-//Dati
 const inputNome = document.getElementById('inputNome');
 const inputKm = document.getElementById('inputKm');
-const inputEta = document.getElementById('inputEta');
+const inputEta = document.getElementById('inputEta')
+const prezzoAlKm = 0.21;
+const prezzoBiglietto = (inputKm * prezzoAlKm)
+let sconto = 0;
+let prezzoFinale;
+let etaPasseggero = document.getElementById(`eta`);
 const bottoneGenera = document.getElementById('btn-input');
 
+bottoneGenera.addEventListener('click', function () {
+  const nome = inputNome.value;
+  const km = inputKm.value;
+  const eta = inputEta.value;
+  // Parte Logica
+
+  document.getElementById(`output-name`).innerHTML = nome
 
 
 
 
+  if (etaPasseggero === `minorenne`) {
+    sconto = 0.2;
+    prezzoFinale = prezzoBiglietto - (prezzoBiglietto * sconto);
+  }
 
+  else if (etaPasseggero === `over65`) {
+    sconto = 0.4;
+    prezzoFinale = prezzoBiglietto - (prezzoBiglietto * sconto);
+  }
+
+  else (etaPasseggero === `maggiorenne`)
+
+  document.getElementById(`prezzo-biglietto`).innerHTML = `${prezzoFinale}`
+
+  // Parte di presentazione
+
+  console.log('Nome ', nome);
+  console.log('KM ', km);
+  console.log('ETA', eta);
+  console.log(`PREZZO`,)
+})
+
+
+/*
 const nomeCognome = prompt("Inserisci nome e cognome");
 const kmDaPercorrere = parseInt("Quanti km vuoi percorrere?");
 const etaPasseggero = parseInt("Quanti anni hai?");
@@ -43,4 +77,4 @@ console.log('Il prezzo del biglietto è ',prezzoFinale)
 output = `
 ${prezzoFinale.toFixed(2)}
 `;
-document.getElementById(`prezzo-biglietto`).innerHTML += output;
+document.getElementById(`prezzo-biglietto`).innerHTML += output;*/
